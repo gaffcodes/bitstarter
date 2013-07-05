25a7b9b99e527ec01ad34f5454b1972e413a60e4
Michael Gaffney <mgaffney1@fordham.edu>
2013-07-04 22:31:12 -0400

    caused web.js to point to index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
+var buf = require('Buffer');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2');
+  var index_buffer = fs.readFileSync('index.html');
+  var index_string = buf.toString(index_buffer);
+  response.send(index_string);
 });
 
 var port = process.env.PORT || 5000;
